d91bf7ba27b77777d085c4bedb5eb446967708a2
dairelad <daireamon@gmail.com>
2022-03-26 08:23:10 +0000

    started data visualisation section

--- run.py
+++ run.py
@@ -30,6 +30,8 @@ parser.add_argument('from_date', type=str, help='"From" date for articles in for
 parser.add_argument('pages', type=int, help='Number of search result pages to return.')
 
 # textblob class used to analyse text for sentiment
+# The polarity score is a float within the range [-1.0, 1.0].
+# The subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective and 1.0 is very subjective.
 def sentiment_analysis(text):
     analysis = TextBlob(text)
     # polarity of a text indicates whether it is positive, negative or neutral
@@ -38,7 +40,7 @@ def sentiment_analysis(text):
 
 def scrape_articles(args):
     # create pandas df with relevant columns
-    article_db = pd.DataFrame(columns=['article_title', 'article_section', 'article_date', 'article_url', 'title_sentiment', 'title_subjectivity'])
+    article_db = pd.DataFrame(columns=['article_title', 'article_section', 'article_date', 'article_url', 'title_sentiment', 'title_subjectivity','sentiment_level','subjectivity level'])
 
     for page in range(1, args.pages+1):
         url = 'https://content.guardianapis.com/search?&q={}&api-key={}&from-date={}&page-size=10&page={}&order-by=relevance'.format(args.keyword, settings.API_KEY, args.from_date, page)
@@ -49,27 +51,69 @@ def scrape_articles(args):
             results = data['response']['results']
             for article in results:
                 date = article['apiUrl'].split('/')
+                # check sentiment (Polarity)
+                if sentiment_analysis(article['webTitle'])[0] < 0:
+                    sent_lvl = 'Negative'
+                elif  sentiment_analysis(article['webTitle'])[0] > 0:
+                    sent_lvl = 'Positive'
+                else:
+                    sent_lvl = 'Neutral'
+                #
+                if sentiment_analysis(article['webTitle'])[1] < 0.5:
+                    sub_lvl = 'Objective'
+                elif  sentiment_analysis(article['webTitle'])[1] > 0.5:
+                    sub_lvl = 'Subjective'
+
                 article_info = {
                 'article_title' : article['webTitle'],
                 'article_section' : article['sectionName'],
                 'article_date' : dateparser.parse('{} {} {}'.format(date[4], date[5], date[6])),
                 'article_url' : article['webUrl'],
                 'title_sentiment' : sentiment_analysis(article['webTitle'])[0],
-                'title_subjectivity' : sentiment_analysis(article['webTitle'])[1]
+                'title_subjectivity' : sentiment_analysis(article['webTitle'])[1],
+                'sentiment_level' : sent_lvl,
+                'subjectivity_level' : sub_lvl
                 }
                 articles.append(article_info)
                 print(article['webTitle'])
                 article_db.loc[len(article_db)] = article_info
     return article_db
 
+def visualise_sentiment(df, args):
+    neg=0; pos=0; neu=0; obj=0; sub=0
+    for index, row in df.iterrows():
+        if row['sentiment_level'] == 'Negative':
+            neg += 1
+        elif row['sentiment_level'] == 'Positive':
+            pos += 1
+        elif row['sentiment_level'] == 'Neutral':
+            neu += 1
+
+        if row['subjectivity_level'] == 'Objective':
+            obj += 1
+        elif row['subjectivity_level'] == 'Subjective':
+            sub += 1
+
+    values = {'neg':neg, 'pos':pos, 'neu':neu, 'obj':obj, 'sub':sub}
+    values = pd.DataFrame(values)
+
+# TODO finish plotting section of code (bar for each series of data)
+    values.plot.hist(grid=True, bins=20, rwidth=0.9,
+                       color='#607c8e')
+    plt.title(f'Sentiment surround the keyword ({args[1]}) in the Guardian headlines')
+    plt.xlabel('Count')
+    plt.ylabel('Commute Time')
+    plt.grid(axis='y', alpha=0.75)
+
+
 if __name__ == '__main__':
     args = parser.parse_args() # checks that the correct arguments have been provided to run the program
     df = scrape_articles(args) # scrapes the guardian website for the relevant articles
     print(df)
 
-# TODO edit this code to provide more information surrounding the sentiment relating to the keyword
-    print(str(df['title_sentiment'].mean()) + ' average sentiment.')
+    visualise_sentiment(df, args)
     df.to_csv('Guardian_{}_sentiment_{}.csv'.format(args.keyword, datetime.datetime.now())) # writes results to csv
+
 
     '''
     Sentiment analysis is a natural language processing technique used to determine whether
